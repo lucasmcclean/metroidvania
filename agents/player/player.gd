7a26ec346f4a172e635d20c8fbad124350a693be
Test Student <leventistifli@gmail.com>
2025-01-21 17:37:22 -0500

    Refactored player script

--- agents/player/player.gd
+++ agents/player/player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+enum States {
+	GROUNDED,
+	AIR
+}
+
 const MAX_WALKING_SPEED:float = 700.0
 const ACCELERATION:float = 5000.0
 
@@ -7,21 +12,28 @@ const ACCELERATION:float = 5000.0
 @onready var collision := $CollisionShape2D as CollisionShape2D
 
 var has_control:bool = false
+var current_state:States = States.GROUNDED
 
-func handle_input(delta: float) -> void:
-	var input_direction:float
-	if has_control:
-		input_direction = Input.get_axis("ui_left", "ui_right")
-	else:
-		input_direction = 0
-	if input_direction != 0:
-		velocity.x += input_direction * ACCELERATION * delta
+func update_velocity(movement_direction:float, delta: float) -> void:
+	if movement_direction != 0:
+		velocity.x += movement_direction * ACCELERATION * delta
 		velocity.x = clamp(velocity.x, -MAX_WALKING_SPEED, MAX_WALKING_SPEED)
 	else:
 		if velocity.x <= (ACCELERATION * delta) && velocity.x >= (-ACCELERATION * delta):
 			velocity.x = 0
 		else:
 			velocity.x -= sign(velocity.x) * ACCELERATION * delta
+
+func ground_movement(delta: float) -> void:
+	if has_control:
+		update_velocity(Input.get_axis("ui_left", "ui_right"), delta)
+	else:
+		update_velocity(0, delta)
+
+func handle_input(delta: float) -> void:
+	match current_state:
+		States.GROUNDED:
+			ground_movement(delta)
 
 func _physics_process(delta: float) -> void:
 	handle_input(delta)

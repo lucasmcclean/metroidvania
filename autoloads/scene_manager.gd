d99b95db76bb0964f51b3eb8dacafba27265fd7d
Lucas McClean <LucksDeveloper@gmail.com>
2025-01-27 08:55:58 -0500

    refactor: break scene_manager switch method into sub-functions, update newlines to adhere to GDScript style guide

--- autoloads/scene_manager.gd
+++ autoloads/scene_manager.gd
@@ -1,26 +1,35 @@
 extends Node
 
-@onready var loading_screen: PackedScene = preload("res://scenes/loading_screen/loading_screen.tscn")
+@onready var _loading_screen: PackedScene = preload("res://scenes/loading_screen/loading_screen.tscn")
 
+
+## Pauses the game and a displays loading screen until [to_scene_path] is loaded.
+## After loading, frees [from_scene]—the scene to be replaced—and resumes the game.
 func switch(from_scene: Node, to_scene_path: StringName) -> void:
 	Game.pause()
 
-	var loading_screen_instance: Node = loading_screen.instantiate()
-	add_child(loading_screen_instance)
-	loading_screen_instance.fade_in()
+	var lc_instance: LoadingScreen = _instantiate_loading_screen()
 
-	await loading_screen_instance.finished()
+	await lc_instance.fade_in()
+
 	from_scene.queue_free()
-	ResourceLoader.load_threaded_request(to_scene_path)
+	_load_and_instantiate_scene(to_scene_path)
 
-	while ResourceLoader.load_threaded_get_status(to_scene_path) != ResourceLoader.THREAD_LOAD_LOADED:
+	await lc_instance.fade_out_and_free()
+	Game.resume()
+
+
+func _instantiate_loading_screen() -> LoadingScreen:
+	var instance: Node = _loading_screen.instantiate()
+	add_child(instance)
+	return instance
+
+
+func _load_and_instantiate_scene(scene_path: StringName) -> void:
+	ResourceLoader.load_threaded_request(scene_path)
+
+	while ResourceLoader.load_threaded_get_status(scene_path) != ResourceLoader.THREAD_LOAD_LOADED:
 		await get_tree().process_frame
 
-	await get_tree().create_timer(10.0).timeout
-	var to_scene: PackedScene = ResourceLoader.load_threaded_get(to_scene_path)
-	Game.add_child(to_scene.instantiate())
-
-	await loading_screen_instance.finished()
-
-	loading_screen_instance.fade_out_and_free()
-	Game.resume()
+	var scene: PackedScene = ResourceLoader.load_threaded_get(scene_path)
+	Game.add_child(scene.instantiate())

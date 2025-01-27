4233250d1af22f6d3b769887828cecf5938244a9
Lucas McClean <LucksDeveloper@gmail.com>
2025-01-27 14:45:27 -0500

    refactor: clean up inconsistencies, remove excess artifacts

--- scenes/agents/player/player.gd
+++ scenes/agents/player/player.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	state_machine.physics_update(delta)
-	move_and_slide()
+	self.move_and_slide()
 
 
 func _process(delta: float) -> void:
@@ -37,27 +37,3 @@ func update_velocity(movement_direction: float, delta: float, acceleration: floa
 			velocity.x = 0
 		else:
 			velocity.x -= sign(velocity.x) * deceleration * delta
-
-
-# var current_state: States = States.GROUNDED
-
-# func update_velocity(movement_direction: float, delta: float) -> void:
-# 	if movement_direction != 0:
-# 		velocity.x += movement_direction * ACCELERATION * delta
-# 		velocity.x = clamp(velocity.x, -MAX_WALKING_SPEED, MAX_WALKING_SPEED)
-# 	else:
-# 		if velocity.x <= (ACCELERATION * delta) && velocity.x >= (-ACCELERATION * delta):
-# 			velocity.x = 0
-# 		else:
-# 			velocity.x -= sign(velocity.x) * ACCELERATION * delta
-
-# func ground_movement(delta: float) -> void:
-# 	if has_control:
-# 		update_velocity(Input.get_axis("ui_left", "ui_right"), delta)
-# 	else:
-# 		update_velocity(0, delta)
-
-# func handle_input(delta: float) -> void:
-# 	match current_state:
-# 		States.GROUNDED:
-# 			ground_movement(delta)

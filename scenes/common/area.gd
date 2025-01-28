6cbdb8328e1e655cc046da7a7a97ad9c1a09cc43
Lucas McClean <LucksDeveloper@gmail.com>
2025-01-28 17:02:49 -0500

    feat: add area generator, area class, update level class
    introduce the area generator which will create an area and randomize level positions within it

---
+++ scenes/common/area.gd
@@ -0,0 +1,3 @@
+class_name Area
+extends Node
+
--- scenes/common/level.gd
+++ scenes/common/level.gd
@@ -5,3 +5,10 @@ extends Node
 @export var limit_top: int
 @export var limit_right: int
 @export var limit_bottom: int
+
+# The positional coordinates where the scene will connect to
+# the prior or next scene.
+@export var connection_start_pos_x: int
+@export var connection_start_pos_y: int
+@export var connection_end_pos_x: int
+@export var connection_end_pos_y: int

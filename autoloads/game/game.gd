002c26f53d64f51ac42d54702972e90711a81db0
Test Student <leventistifli@gmail.com>
2025-01-28 15:59:53 -0500

    Added fullscreen controls

--- autoloads/game/game.gd
+++ autoloads/game/game.gd
@@ -7,3 +7,11 @@ func pause() -> void:
 
 func resume() -> void:
 	get_tree().paused = false
+
+func _process(_delta: float) -> void:
+	if(Input.is_action_just_pressed("fullscreen")):
+		if(get_window().mode == Window.MODE_WINDOWED):
+			get_window().mode = Window.MODE_FULLSCREEN
+		else:
+			get_window().mode = Window.MODE_WINDOWED
+			get_window().size = Vector2(1920, 1080)

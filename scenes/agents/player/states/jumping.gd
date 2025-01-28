21c1b0cb93f1467b4f34387807d7229181482ebb
Test Student <leventistifli@gmail.com>
2025-01-28 17:06:04 -0500

    Added jumping and falling

--- scenes/agents/player/states/jumping.gd
+++ scenes/agents/player/states/jumping.gd
@@ -1,1 +1,20 @@
 extends PlayerState
+
+@export var fall_state: PlayerState
+@export var ground_state: PlayerState
+
+func enter() -> void:
+	super()
+	player.velocity.y += player.JUMP_VELOCITY
+
+func _check_falling() -> void:
+	if player.velocity.y >= 0:
+		state_machine.change_state(fall_state)
+
+func physics_update(delta: float) -> void:
+	player.apply_gravity(delta)
+	if player.has_control:
+		player.update_velocity(player.get_input_direction(), delta, player.GROUNDED_ACCELERATION, player.GROUNDED_ACCELERATION)
+	else:
+		player.update_velocity(0, delta, player.GROUNDED_ACCELERATION, player.GROUNDED_ACCELERATION)
+	_check_falling()

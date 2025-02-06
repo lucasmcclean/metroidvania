8ca2f081d08fae1b1c0cf5698673c9981f3568b8
RasheedC131 <ra357094@ucf.edu>
2025-02-06 16:35:02 -0500

    Combo Testing and Stuff I need to delete later

--- autoloads/combo_manager/combo_manager.gd
+++ autoloads/combo_manager/combo_manager.gd
@@ -8,15 +8,15 @@ var score: int = 0
 @onready var timer := $Timer as Timer
 
 
-func start_combo():
+func start_combo() -> void:
 	timer.start(timeLimit)
 
 
-func add_score(amount: int):
+func add_score(amount: int) -> void:
 	score += amount
 
 
-func add_time(amount: int):
+func add_time(amount: int) -> void:
 	#Checking if Combo has already Started
 	if(timer.time_left > 0):
 		#Makes sure time doesnt go over limit
@@ -25,7 +25,7 @@ func add_time(amount: int):
 		start_combo()
 
 
-func take_time(amount: int):
+func take_time(amount: int) -> void:
 	if (timer.time_left - amount > 0):
 		timer.start(timer.time_left - amount)
 	else:
@@ -33,13 +33,13 @@ func take_time(amount: int):
 		#Possible to add penelty for going negative time
 
 
-func pause():
+func pause() -> void:
 	timer.paused = true
 
 
-func unpause():
+func unpause() -> void:
 	timer.paused = false
 
 
-func _on_timer_timeout():
+func _on_timer_timeout() -> void:
 	score = 0
--- scenes/agents/player/combo_ui.gd
+++ scenes/agents/player/combo_ui.gd
@@ -8,15 +8,22 @@ var comboScore: int
 
 func _ready() -> void:
 	comboTimerDisplay.max_value = comboTimer.wait_time
+	Combo.add_score(1)
+	Combo.add_time(2)
 
 func _process(_delta: float) -> void:
 	comboScore = Combo.score
 	comboTimerDisplay.value = comboTimer.time_left
-	
-	
 	comboScoreDisplay.text = str(comboScore)
+	
 	if (comboScore <= 0):
 		comboScoreDisplay.visible = false
 		pass
 	else:
 		comboScoreDisplay.visible = true
+	
+	if (comboTimer.time_left <= 0):
+		comboTimerDisplay.visible = false
+		pass
+	else:
+		comboTimerDisplay.visible = true

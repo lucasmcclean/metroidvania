03ec7a24864f4f301b1693a86bbad0f1f7057aac
Lucas McClean <LucksDeveloper@gmail.com>
2025-02-17 08:14:01 -0500

    fix: correct slime layers and mask, static types and _ private vars

--- scenes/agents/enemy/slime/slime.gd
+++ scenes/agents/enemy/slime/slime.gd
@@ -1,95 +1,96 @@
 class_name Slime
 extends CharacterBody2D
 
-const speed = 50
-var is_slime_chase: bool = true
+const _speed = 50
+var _is_slime_chase: bool = true
 
-var health = 10
-var health_max = 80
-var health_min = 0
+# var _health: int = 10
+# var _health_max: int = 80
 
-var dead: bool = false
-var taking_damage: bool = false
-var dealt_damage = 10
-var is_dealing_damage: bool = false
+var _dead: bool = false
+var _taking_damage: bool = false
+# var _dealt_damage: int = 10
+var _is_dealing_damage: bool = false
 
-var dir: Vector2
-var dirCopy: Vector2
-const gravity = 900
-var knockback_force = -200
-var is_roaming: bool = true
-var player_in_area = false
+var _dir: Vector2
+var _dir_copy: Vector2
+const _gravity = 900
+var _knockback_force: int = -200
+var _is_roaming: bool = true
+# var _player_in_area: bool = false
 
-		
-func _process(delta):
+@onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _process(_delta: float) -> void:
 	if !is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += _gravity * _delta
 		velocity.x = 0
-	move(delta)
-	handle_animation()
+	_move(_delta)
+	_handle_animation()
 	move_and_slide()
 
-func move(delta):
-	if !dead:
-		if !is_slime_chase:
-				velocity += dir * speed * delta
-		elif is_slime_chase and !taking_damage:
+
+func _move(_delta: float) -> void:
+	if !_dead:
+		if !_is_slime_chase:
+			velocity += _dir * _speed * _delta
+		elif _is_slime_chase and !_taking_damage:
 			$JumpTimer.wait_time = 2
-			var dir_to_player = (Global.player_position - global_position).normalized()
+			var _dir_to_player: Vector2 = (Global.player_position - global_position).normalized()
 			if !is_on_floor():
-				velocity.x = dir_to_player.x * 200
-				dir.x = abs(velocity.x) / velocity.x
+				velocity.x = _dir_to_player.x * 200
+				_dir.x = abs(velocity.x) / velocity.x
 			else:
 				velocity.x = 0
-		elif taking_damage:
-			var knockback_dir = position.direction_to(Global.player_position) * knockback_force
-			velocity.x = knockback_dir.x
-		is_roaming = true
-	elif dead:
+		elif _taking_damage:
+			var _knockback_dir: Vector2 = position.direction_to(Global.player_position) * _knockback_force
+			velocity.x = _knockback_dir.x
+		_is_roaming = true
+	elif _dead:
 		velocity.x = 0
 
-func handle_animation():
-	var anim_sprite = $AnimatedSprite2D
-	if !dead and !taking_damage and !is_dealing_damage:
-		anim_sprite.play("walk")
-		if dir.x == -1:
-			anim_sprite.flip_h = true
-		elif dir.x == 1:
-			anim_sprite.flip_h = false 
-	elif !dead and taking_damage and !is_dealing_damage:
-		anim_sprite.play("hurt")
-		await get_tree().create_timer(.08).timeout
-		taking_damage = false
-	elif dead and is_roaming:
-		is_roaming = false
-		anim_sprite.play("death")
-		await get_tree().create_timer(1.0).timeout
-		handle_death()
-		
 
-func handle_death():
+func _handle_animation() -> void:
+	if !_dead and !_taking_damage and !_is_dealing_damage:
+		_sprite.play("walk")
+		if _dir.x == -1:
+			_sprite.flip_h = true
+		elif _dir.x == 1:
+			_sprite.flip_h = false 
+	elif !_dead and _taking_damage and !_is_dealing_damage:
+		_sprite.play("hurt")
+		await get_tree().create_timer(.08).timeout
+		_taking_damage = false
+	elif _dead and _is_roaming:
+		_is_roaming = false
+		_sprite.play("death")
+		await get_tree().create_timer(1.0).timeout
+		_handle_death()
+
+
+func _handle_death() -> void:
 	self.queue_free()
 
+
 func _on_direction_timer_timeout() -> void:
-	$DirectionTimer.wait_time = choose([1.5, 2.0, 2.5])
-	if !is_slime_chase:
-		dirCopy = dir
-		dir = choose([Vector2.RIGHT, Vector2.LEFT])
-		if dir != dirCopy:
+	$DirectionTimer.wait_time = [1.5, 2.0, 2.5].pick_random()
+	if !_is_slime_chase:
+		_dir_copy = _dir
+		_dir = [Vector2.RIGHT, Vector2.LEFT].pick_random()
+		if _dir != _dir_copy:
 			velocity.x = 0
-		
 
-func choose(array):
-	array.shuffle()
-	return array.front()
 
-func _on_slime_hitbox_area_entered(area: Area2D) -> void:
-	var damage = Global.player_damage
-	##To be implemented once the attack is created
+func _on_slime_hitbox_area_entered(_area: Area2D) -> void:
+	## Talk this over with Stovehead because I believe that each area may handle
+	## its own damage value rather than there being a global
+	var _damage: int = Global.player_damage
+	## To be implemented once the attack is created
 
 
 func _on_jump_timer_timeout() -> void:
-	if is_slime_chase:
+	if _is_slime_chase:
 		velocity.y = -600
-		var dir_to_player = (Global.player_position - global_position).normalized()
-		velocity.x = dir_to_player.x * 100
+		var _dir_to_player: Vector2 = (Global.player_position - global_position).normalized()
+		velocity.x = _dir_to_player.x * 100

c8701a7a76c5f9c354ef29bea5c3e258aaec36ac
Lucas McClean <LucksDeveloper@gmail.com>
2025-02-20 14:33:08 -0500

    feat: add guard room, castle zone generator, update tile size

--- scenes/_game_launcher/game_launcher.gd
+++ scenes/_game_launcher/game_launcher.gd
@@ -2,7 +2,13 @@ extends Node
 
 @export var initial_scene: PackedScene
 
+@onready var player_scene: PackedScene = preload("res://scenes/agents/player/player.tscn")
+
 
 func _ready() -> void:
 	Game.add_child(initial_scene.instantiate())
+	var player: Player = player_scene.instantiate() as Player
+	Game.add_child(player)
+	player.global_position.y -= 200
+
 	self.queue_free()

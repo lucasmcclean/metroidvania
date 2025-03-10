8dce1d9d9a9f95effe5b3191d1a42daa3e598757
Stovehead <leventistifli@gmail.com>
2025-03-10 12:58:01 -0400

    Fixed some formatting...

--- scenes/agents/enemy/slime/slime.gd
+++ scenes/agents/enemy/slime/slime.gd
@@ -6,20 +6,13 @@ const SPEED: float = 50.0
 const DIRECTION_WAIT_TIMES: Array[float] = [1.5, 2.0, 2.5]
 
 var _is_slime_chase: bool = true
-
-# var _health: int = 10
-# var _health_max: int = 80
-
 var _dead: bool = false
 var _taking_damage: bool = false
-# var _dealt_damage: int = 10
 var _is_dealing_damage: bool = false
-
 var _dir: Vector2
 var _dir_copy: Vector2
 var _knockback_force: int = -200
 var _is_roaming: bool = true
-# var _player_in_area: bool = false
 
 ## Change to animation player regular sprite
 @onready var _sprite := $AnimatedSprite2D as AnimatedSprite2D
@@ -47,7 +40,7 @@ func _move(_delta: float) -> void:
 			var _dir_to_player: Vector2 = (Global.player_position - global_position).normalized()
 			if !is_on_floor():
 				velocity.x = _dir_to_player.x * 200
-				_dir.x = abs(velocity.x) / velocity.x
+				_dir.x = absf(velocity.x) / velocity.x
 			else:
 				velocity.x = 0
 		elif _taking_damage:
@@ -67,24 +60,27 @@ func _handle_animation() -> void:
 			_sprite.flip_h = false 
 	elif !_dead and _taking_damage and !_is_dealing_damage:
 		_sprite.play("hurt")
-		await get_tree().create_timer(.08).timeout
+		await get_tree().create_timer(.08, true, true).timeout
 		_taking_damage = false
 	elif _dead and _is_roaming:
 		_is_roaming = false
 		_sprite.play("death")
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(1.0, true, true).timeout
 		_handle_death()
 
 
 func _handle_death() -> void:
 	self.queue_free()
 
-## TODO: Change to variable
+
 func _on_direction_timer_timeout() -> void:
-	_direction_timer.wait_time = DIRECTION_WAIT_TIMES.pick_random()
+	_direction_timer.wait_time = DIRECTION_WAIT_TIMES[randi() % DIRECTION_WAIT_TIMES.size()]
 	if !_is_slime_chase:
 		_dir_copy = _dir
-		_dir = [Vector2.RIGHT, Vector2.LEFT].pick_random()
+		if randi() % 2 == 0:
+			_dir = Vector2.LEFT
+		else:
+			_dir = Vector2.RIGHT
 		if _dir != _dir_copy:
 			velocity.x = 0
 
@@ -94,11 +90,11 @@ func _on_slime_hitbox_area_entered(_area: Area2D) -> void:
 	## its own damage value rather than there being a global
 	var _damage: int = Global.player_damage
 	## To be implemented once the attack is created
-	
-	
+
 
 func _on_slime_hit(attacker: Hurtbox) -> void:
 	print(_health_component._remaining_hp)
+
 
 func _on_jump_timer_timeout() -> void:
 	if _is_slime_chase:
